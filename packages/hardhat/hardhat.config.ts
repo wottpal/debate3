839271c3a63d719d1ea011125bf862e3f6c2ffe5
CarlosR <carlos.ramos@grupoimportia.com>
2022-07-22 13:13:52 +0200

    sc setup

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -2,49 +2,121 @@ import '@nomicfoundation/hardhat-toolbox'
 import '@nomiclabs/hardhat-vyper'
 import * as dotenv from 'dotenv'
 import { HardhatUserConfig } from 'hardhat/config'
-import path from 'path'
-dotenv.config()
+import * as ethers from 'ethers'
+import '@nomiclabs/hardhat-etherscan'
+import '@nomiclabs/hardhat-waffle'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+import 'hardhat-deploy'
+import 'hardhat-contract-sizer'
+import 'hardhat-deploy-ethers'
+import '@openzeppelin/hardhat-upgrades'
 
-const config: HardhatUserConfig = {
-  solidity: '0.8.9',
-  vyper: {
-    version: '0.3.3',
-  },
-  paths: {
-    artifacts: path.resolve('../frontend/src/artifacts'),
+const mnemonic = 'replace hover unaware super where filter stone fine garlic address matrix basic'
+
+const privateKeys = []
+
+//creates the list of private keys from the mnemonic
+const derivePath = "m/44'/60'/0'/0/"
+for (let i = 0; i <= 10; i++) {
+  const wallet = new (ethers.Wallet.fromMnemonic as any)(mnemonic, `${derivePath}${i}`)
+  privateKeys.push(wallet.privateKey)
+}
+
+module.exports = {
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
   },
   networks: {
     hardhat: {
+      accounts: {
+        mnemonic,
+      },
       chainId: 1337,
-      //   chainId: 80001,
-      //   allowUnlimitedContractSize: false,
-      //   blockGasLimit: 20000000, // 20 million
-      //   forking: {
-      //     url: "https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_MUMBAI}",
-      //     url: "https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_RINKEBY}",
-      //     url: "https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_MAINNET}",
-      //   },
+      initialBaseFeePerGas: 0,
     },
-    rinkeby: {
-      chainId: 4,
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_RINKEBY}`,
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+      chainId: 1337,
+      timeout: 6000000,
+    },
+    mainnet: {
+      url: `${process.env.MAINNET_PROVIDER_URL}`,
       accounts: [
-        ...(process.env.PRIVATE_KEY_RINKEBY ? [`${process.env.PRIVATE_KEY_RINKEBY}`] : []),
+        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
       ],
+      chainId: 1,
     },
-    mumbai: {
+    skale: {
+      url: `${process.env.SKALE_PROVIDER_URL}`,
+      accounts: [
+        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+      ],
+      chainId: 0xafcee83030b95,
+    },
+    polygonMumbai: {
+      url: `${process.env.MUMBAI_PROVIDER_URL}`,
+      accounts: [
+        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+      ],
       chainId: 80001,
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_MUMBAI}`,
-      accounts: [...(process.env.PRIVATE_KEY_MUMBAI ? [`${process.env.PRIVATE_KEY_MUMBAI}`] : [])],
     },
+    polygon: {
+      url: `${process.env.POLYGON_PROVIDER_URL}`,
+      accounts: [
+        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+      ],
+      chainId: 137,
+    },
+    bscTestnet: {
+      url: `${process.env.BSCTEST_PROVIDER_URL}`,
+      accounts: [
+        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+      ],
+      chainId: 97,
+    },
+    bsc: {
+      url: `${process.env.BSC_PROVIDER_URL}`,
+      accounts: [
+        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+      ],
+      chainId: 56,
+    },
+  },
+  namedAccounts: {
+    deployerAddr: {
+      default: 0,
+      localhost: 0,
+    },
+    governorAddr: {
+      default: 1,
+      localhost: 1,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
   },
   etherscan: {
-    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+      bscTestnet: process.env.BSCSCAN_API_KEY,
+      polygon: process.env.POLYGONSCAN_API_KEY,
+    },
   },
-  // NOTE: typechain command is called manually as it currently ignores vyper contracts
-  // typechain: {
-  //   outDir: path.resolve('../frontend/src/types/typechain'),
-  // },
 }
-
-export default config
