374c2e3ec2a76f621dd27d4073f53d5684a926c9
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2022-07-22 14:55:54 +0200

    frontend: add orbis provider

---
+++ packages/frontend/src/components/OrbisProvider.tsx
@@ -0,0 +1,21 @@
+import { Orbis } from '@orbisclub/orbis-sdk'
+import { createContext, FC, PropsWithChildren, useContext, useEffect, useState } from 'react'
+
+export type OrbisProviderContextType = {
+  orbis?: any
+}
+export const OrbisContext = createContext<OrbisProviderContextType>({})
+
+export const useOrbisContext = () => {
+  return useContext(OrbisContext)
+}
+
+export const PrivyClientProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [orbis, setOrbis] = useState(null)
+
+  useEffect(() => {
+    setOrbis(new Orbis())
+  }, [])
+
+  return <OrbisContext.Provider value={{ orbis }}>{children}</OrbisContext.Provider>
+}
