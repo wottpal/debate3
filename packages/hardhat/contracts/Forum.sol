0d11d0d96241b6ff0b915099af5b3810b1c57b5c
CarlosR <carlos.ramos@grupoimportia.com>
2022-07-23 12:13:13 +0200

    Feat : badges can be exchanged Feat : test for airdrop

--- packages/hardhat/contracts/Forum.sol
+++ packages/hardhat/contracts/Forum.sol
@@ -1,27 +1,34 @@
 pragma solidity ^0.8.9;
 import './IMembership.sol';
 import "hardhat/console.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Forum {
+
+
+contract Forum is Ownable{
+
+    using Counters for Counters.Counter;
 
     uint256  public moderatorsCount;
     bool public initialized;
     address immutable membershipNFT;
     string public baseURI;
-    mapping (address => bool) isModerator;
+    mapping (address => bool) _isModerator;
+    mapping (address => uint256) public contributionScore;
+    mapping (uint256 => address) public _contributorByIndex;
+    Counters.Counter public _contributorCounter;
 
     modifier onlyModerator(){
-        require(isModerator[msg.sender],"Caller is not allowed to mint");
+        require(_isModerator[msg.sender],"Caller is not allowed to mint");
         _;
     }
-
-
     constructor (address[] memory _moderators, address _membershipNFT, string memory _baseURI){
         require (initialized != true , "already initialized");
         uint256 len = _moderators.length;
         membershipNFT = _membershipNFT;
         for (uint256 i = 0 ; i < len ; i ++){
-            isModerator[_moderators[i]] = true;
+            _isModerator[_moderators[i]] = true;
         }
         initialized = true;    
         baseURI = _baseURI;
@@ -35,7 +42,27 @@ contract Forum {
     function revokeMembership(address[] memory users) onlyModerator external  {
         IMembership(membershipNFT).revokeMembership(users);
     }
+    function addContribution(address user, uint256 score) onlyOwner external {
+        if(contributionScore[user]==0){
+            uint256 currIdx = _contributorCounter.current();
+            _contributorCounter.increment();
+            _contributorByIndex[currIdx] = user;
+        }
+        contributionScore[user] = contributionScore[user]+score;
+    }
+    function isModerator(address _moderator) external view returns(bool){
+        return(_isModerator[_moderator]);
+    }
+    function getContributors() external view returns(address[]memory, uint256[] memory){
+        uint256 currIdx = _contributorCounter.current();
+        address[] memory users = new address[](currIdx);
+        uint256[] memory scores = new uint256[](currIdx);
 
-
+        for(uint256 i = 0; i < currIdx ; i++){
+            users[i] = _contributorByIndex[i];
+            scores[i] = contributionScore[_contributorByIndex[i]];
+        }
+        return(users,scores);
+    }
 
 }
